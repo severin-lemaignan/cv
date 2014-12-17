52830e76467aafb8d2514530765c1809b21a7291
Séverin Lemaignan <severin.lemaignan@epfl.ch>
2014-12-17 15:01:48 +0100

    Updated the list of publications
    While here, made a few fix to 'generate_publication'. Note that the resulting
    .tex should be processed with lualatex to prevent utf8 issues

--- generate_publications.py
+++ generate_publications.py
@@ -1,12 +1,24 @@
 #! /usr/bin/env python
+# -*- coding: utf-8 -*-
 
+import sys,codecs, locale
+sys.stdout = codecs.getwriter(locale.getpreferredencoding())(sys.stdout)
+import bibtexparser
 from bibtexparser.bparser import BibTexParser
-from bibtexparser.customization import author, getnames, homogeneize_latex_encoding
+from bibtexparser.customization import author, getnames, convert_to_unicode 
 
-preamble = r"""
-\documentclass[10pt,a4paper]{moderncv}
+phd_preamble = "Dissertation"
+journal_preamble = "International peer-reviewed journals"
+book_preamble = "Book chapters"
+conf_preamble = "International peer-reviewed conference article (6-8 pages)"
+short_preamble = "Short peer-reviewed publications"
+
+
+##############################################
+##############################################
+tex_preamble = r"""
+\documentclass[10pt,a4paper,sans]{moderncv}
 \moderncvtheme[green]{classic}
-\usepackage[utf8]{inputenc}
 \usepackage[scale=0.8]{geometry}
 \usepackage{url}
 \setlength{\hintscolumnwidth}{1.5cm}
@@ -15,25 +27,47 @@ preamble = r"""
 \familyname{}
 \begin{document}
 \section{Publications}
+\emph{h-index: 11} (source: Google Scholar)
 """
 
-endamble = r"""
+tex_endamble = r"""
 \end{document}
 """
+##############################################
+##############################################
+html_templates = dict()
 
-phd_preamble = "Dissertation"
-journal_preamble = "International peer-reviewed journals"
-book_preamble = "Book chapters"
-conf_preamble = "International peer-reviewed conference article (6-8 pages)"
-short_preamble = "Short peer-reviewed publications"
+html_templates[journal_preamble] = r"""
+<li><b><a href="publis/{id}.pdf" alt="Download the PDF version">
+{title}</a></b>, <i>{journal}</i>, {year} <a onclick="$('#bibtex_{id}').show('slow');">(show bibtex)</a><br/>
+<b>{author}</b>
+<pre style="display: none" id="bibtex_{id}">
+"""
+
+html_templates[conf_preamble] = r"""
+<li><b><a href="publis/{id}.pdf" alt="Download the PDF version">
+{title}</a></b>, <i>{booktitle}</i>, {year} <a onclick="$('#bibtex_{id}').show('slow');">(show bibtex)</a><br/>
+<b>{author}</b>
+<pre style="display: none" id="bibtex_{id}">
+"""
+
+html_endamble = r"""
+}}
+</pre>
+</li>
+"""
+##############################################
+##############################################
 
 bp = None
 
 with open("publications.bib", 'r') as bibfile:
     #bp = BibTexParser(bibfile, customization=homogeneize_latex_encoding)
-    bp = BibTexParser(bibfile)
+    parser = BibTexParser()
+    parser.customization = convert_to_unicode
+    bp = bibtexparser.load(bibfile, parser=parser)
 
-publis = bp.get_entry_list()
+publis = bp.entries
 
 shorts = sorted([a for a in publis if ('note' in a and 'Short' in a['note'])], key=lambda i: i["year"], reverse = True)
 
@@ -47,7 +81,7 @@ journals = sorted([a for a in publis if a['type']  in ['article']], key=lambda i
 
 bookchapters = sorted([a for a in publis if a['type']  in ['inbook']], key=lambda i: i['year'], reverse = True)
 
-def format(preamble, items, book = False):
+def tex_format(preamble, items, book = False):
     res  = r"\vspace{1em}\subsection{" + preamble + "}\n"
 
     if book:
@@ -56,6 +90,9 @@ def format(preamble, items, book = False):
         return res
 
     for i in items:
+        if 'note' in i and "Under review" in i['note']:
+            continue
+
         booktitle = ""
         if 'booktitle' in i:
             booktitle = i["booktitle"]
@@ -76,10 +113,62 @@ def format(preamble, items, book = False):
 
     return res
 
-print(preamble)
-print(format(journal_preamble, journals))
-print(format(conf_preamble, confs))
-print(format(short_preamble, shorts))
-print(format(book_preamble, bookchapters, book = True))
-print(format(phd_preamble, dissertation))
-print(endamble)
+def html_format(preamble, items, book = False):
+    res  = r"<h4>" + preamble + "</h4>\n<ul>"
+
+    #if book:
+    #    for i in items:
+    #        res += "\\vspace{0.2em}\\cvlistitem{%s \\\\ \\textbf{%s} \\\\ in:\\textit{%s} %s.}\n" % (i["author"], i["chapter"], i["title"], i["year"])
+    #    return res
+
+    for i in items:
+        import pdb;pdb.set_trace()
+        res += html_templates[preamble].format(**i)
+
+        booktitle = ""
+        if 'booktitle' in i:
+            booktitle = i["booktitle"]
+        elif 'journal' in i:
+            booktitle = i["journal"]
+
+        pub = ""
+        if 'isbn' in i:
+            pub = " ISBN:~%s." % i['isbn']
+        elif 'issn' in i:
+            pub = " ISSN:~%s." % i['issn']
+
+        if 'note' in i and i['note'] != "Short":
+            pub += " %s." % (", ".join([t for t in i['note'].split("Short,") if t]).strip())
+
+        res += i['raw']
+        res += html_endamble
+
+    return res + "\n</ul>\n"
+
+#
+#@article{{{key},
+#author={{Lemaignan, Séverin}},
+#title={{Grounding the Interaction: Knowledge Management for Interactive Robots}},
+#year={{2013}},
+#issn={{0933-1875}},
+#journal={{KI - Künstliche Intelligenz}},
+#volume={{27}},
+#number={{2}},
+#doi={{10.1007/s13218-013-0246-3}},
+#publisher={{Springer-Verlag}},
+#pages={{183-185}},
+#
+
+print(tex_preamble)
+sys.stdout.write(tex_format(journal_preamble, journals))
+print(tex_format(conf_preamble, confs))
+print(tex_format(short_preamble, shorts))
+print(tex_format(book_preamble, bookchapters, book = True))
+print(tex_format(phd_preamble, dissertation))
+print(tex_endamble)
+
+#print(html_format(journal_preamble, journals))
+#print(html_format(conf_preamble, confs))
+#print(html_format(short_preamble, shorts))
+#print(html_format(book_preamble, bookchapters, book = True))
+#print(html_format(phd_preamble, dissertation))
